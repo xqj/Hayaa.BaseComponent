6764a07527cbad4bb10341cf5740c791367d874b
hsieh jing <xqj198404@gmail.com>
2016-08-28 16:34:40 +0800

    完成

--- Hayaa.BaseComponetLib/Hayaa.BaseComponent.DataAccess/Core/MysqlService.cs
+++ Hayaa.BaseComponetLib/Hayaa.BaseComponent.DataAccess/Core/MysqlService.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using Hayaa.BaseComponent.DataAccess.Config;
 using System.Data;
+using Dapper;
 
 namespace Hayaa.BaseComponent.DataAccess
 {
@@ -12,22 +13,23 @@ namespace Hayaa.BaseComponent.DataAccess
     {
         public int ExecuteNonQuery<P>(string connectionString, string cmdText, P paramater, CommandType cmdType = CommandType.Text)
         {
-            throw new NotImplementedException();
+            IDbConnection con = new MySqlConnection(connectionString);
+            int r = con.Execute(cmdText, paramater, null, null, cmdType);
+            return r;
         }
 
         public T GetData<T, P>(string connectionString, string cmdText, P paramater, CommandType cmdType = CommandType.Text)
         {
-            throw new NotImplementedException();
+            IDbConnection con = new MySqlConnection(connectionString);
+            var r = con.QuerySingle<T>(cmdText, paramater, null, null, cmdType);
+            return r;
         }
 
         public List<T> GetList<T, P>(string connectionString, string cmdText, P paramater, CommandType cmdType = CommandType.Text)
         {
-            throw new NotImplementedException();
-        }
-
-        public int GetPrimarykey<P>(string connectionString, string cmdText, P paramater, CommandType cmdType = CommandType.Text)
-        {
-            throw new NotImplementedException();
+            IDbConnection con = new MySqlConnection(connectionString);
+            var r = con.Query<T>(cmdText, paramater, null, true, null, cmdType).ToList();
+            return r;
         }
     }
 }
